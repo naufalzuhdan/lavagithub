14eb455fc7d8e7f472b8b855551514f1b616058b
Muhamad naufal zuhdan nawa <60124823+naufalzuhdan@users.noreply.github.com>
2024-03-26 08:29:10 +0700

    run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-41532694-ca5c-4dd5-b7e5-7188e6fb3ea3/)
 
 
 echo "$curl_output" >> Update.tsx
